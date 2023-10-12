c95b0197ac8101744983493445513ab305c67db3
Mircea Casapu <mircea.casapu@gmail.com>
2023-10-12 20:00:00 +0300

    feat: setup mui with tailwind

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,10 @@
+import { Slider } from '@mui/material';
+
 export default function Login() {
   return (
-    <>
-      <p>Supabase</p>
-    </>
+    <div>
+      <Slider defaultValue={30} />
+      <Slider defaultValue={30} className="text-teal-600" />
+    </div>
   );
 }
